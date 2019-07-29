08a8544488e24311e9794a5c335c0b094ef96321
birkin <birkin.diana@gmail.com>
2019-07-29 08:10:43 -0400

    refactors callnumber reference.

--- book_locator_app/lib/view_map_helper.py
+++ book_locator_app/lib/view_map_helper.py
@@ -13,24 +13,24 @@ def parse_request( querydct ):
         Called by views.map() """
     log.debug( f'querydct, ```{pprint.pformat(querydct)}```' )
     location = querydct.get( 'loc', None )
-    call_number = querydct.get( 'call', None )
+    callnumber = querydct.get( 'call', None )
     status = querydct.get( 'status', None )
     title = querydct.get( 'title', None )
-    ( location, call_number, status, title ) = apply_unquote( location, call_number, status, title )
-    log.debug( f'location, `{location}`; call_number, `{call_number}`; status, `{status}`; title, `{title}`' )
-    return ( location, call_number, status, title )
+    ( location, callnumber, status, title ) = apply_unquote( location, callnumber, status, title )
+    log.debug( f'location, `{location}`; callnumber, `{callnumber}`; status, `{status}`; title, `{title}`' )
+    return ( location, callnumber, status, title )
 
-def apply_unquote( location, call_number, status, title ):
+def apply_unquote( location, callnumber, status, title ):
     """ Unquotes field.
         Called by parse_request() """
     if location:
         location = unquote( location )
-    if call_number:
-        call_number = unquote( call_number )
+    if callnumber:
+        callnumber = unquote( callnumber )
     if status:
         status = unquote( status )
     if title:
         title = unquote( title )
-    # log.debug( f'location, `{location}`; call_number, `{call_number}`; status, `{status}`; title, `{title}`' )
+    # log.debug( f'location, `{location}`; callnumber, `{callnumber}`; status, `{status}`; title, `{title}`' )
     log.debug( 'unquote applied' )
-    return ( location, call_number, status, title )
+    return ( location, callnumber, status, title )
