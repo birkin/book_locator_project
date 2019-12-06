7ddcc8372b0fad4ffedf3a1acbe1f2b42fcc0e31
birkin <birkin.diana@gmail.com>
2019-12-06 11:10:40 -0500

    finds and extracts duplicates.

--- book_locator_app/lib/label_helper.py
+++ book_locator_app/lib/label_helper.py
@@ -20,7 +20,9 @@ def arrange_metadata_by_floor( data_code ):
     assert data_code in ['rock', 'sci', 'chinese', 'japanese', 'korean']
     initial_dct = load_json( data_code )
     sorted_floor_list = prep_floor_list( initial_dct )  # TODO: I can do lots more work in this single-pass
-    range_data_by_floor = prep_floor_ranges( sorted_floor_list, initial_dct )
+    floor_dct = prep_floor_ranges( sorted_floor_list, initial_dct )
+    duplicates = find_duplicates( floor_dct )
+    ( updated_floor_dct, updated_duplicates ) = extract_duplicates( floor_dct, duplicates )
 
 def load_json( data_code ):
     """ Loads appropriate json file.
@@ -47,7 +49,8 @@ def prep_floor_list( initial_dct ):
     return sorted_floor_list
 
 def prep_floor_ranges( sorted_floor_list, initial_dct ):
-    """ Preps ranges for each floor.
+    """ Attaches range-dct to floor-key for each floor.
+        Example output: { 'a': [(aisle-1a-range-dct), (aisle-1b-range-dct)], 'b': ... }
         Called by arrange_metadata_by_floor() """
     floor_dct = {}
     for floor in sorted_floor_list:
@@ -55,26 +58,92 @@ def prep_floor_ranges( sorted_floor_list, initial_dct ):
     for ( normalized_cn_key, range_info_dct) in initial_dct.items():
         if range_info_dct['floor']:
             floor = str( range_info_dct['floor'] )
-            # range_info_dct['normalized_callnumber'] = normalized_cn_key
             range_info_dct['padded_aisle'] = range_info_dct['aisle'].zfill( 5 )  # for possible sorting later
-            floor_dct[floor].append( range_info_dct )
-        # if len( floor_dct[floor] ) > 100:
-        #     break
-    log.debug( f'initial floor_dct, ```{pprint.pformat(floor_dct)}```' )
-    # for ( floor_key, range_dct_lst ) in floor_dct.items():
-    #     # sorted_range_dct_lst = sorted( range_dct_lst, key=itemgetter('normalized_start') )
-    #     sorted_range_dct_lst = sorted( range_dct_lst, key=itemgetter('padded_aisle', 'normalized_start') )
-    #     floor_dct[floor_key] = sorted_range_dct_lst
 
-    for ( floor_key, range_dct_lst ) in floor_dct.items():
-        duplicate_aisle_check = []
-        for range_dct in range_dct_lst:
-            if range_dct['aisle'] not in duplicate_aisle_check:
-                duplicate_aisle_check.append( range_dct['aisle'] )
-            else:
-                range_dct['duplicate_aisle'] = True
+            ## data cleanup
+            stripped_aisle = range_info_dct['aisle'].strip()
+            if stripped_aisle != range_info_dct['aisle']:
+                log.debug( 'updating aisle with stripped version' )
+                range_info_dct['aisle'] = stripped_aisle
+            if 'location_code' not in range_info_dct.keys() and 'location-code' in range_info_dct.keys():
+                range_info_dct['location_code'] = range_info_dct['location-code']
+                del range_info_dct['location-code']
+
+            floor_dct[floor].append( range_info_dct )
+            # if len( floor_dct[floor] ) < 20:
+            #     floor_dct[floor].append( range_info_dct )
+    log.debug( f'floor_dct, ```{pprint.pformat(floor_dct)}```' )
+    return floor_dct
+
 
     log.debug( f'final floor_dct, ```{pprint.pformat(floor_dct)}```' )
     return floor_dct
 
+def find_duplicates( floor_dct ):
+    """ Finds the "multiple-aisle" entries and returns them.
+        Called by arrange_metadata_by_floor() """
+    all_duplicates_check = []  # will add, eg, {'floor': 'a'; 'aisle': '12b'}
+    for ( floor_key, range_dct_lst ) in floor_dct.items():
+        floor_duplicates_check = []
+        for ( i, range_dct ) in enumerate( range_dct_lst ):  # ah, not using the index as I thought I would
+            if range_dct['aisle'] not in floor_duplicates_check:
+                floor_duplicates_check.append( range_dct['aisle'] )
+            else:
+                range_dct['duplicate_aisle'] = True  # not necessary for processing, but useful for visual check
+                duplicate = {'floor': range_dct['floor'], 'aisle': range_dct['aisle']}
+                all_duplicates_check.append( duplicate )
+    log.debug( f'duplicates, ```{pprint.pformat(all_duplicates_check)}```' )
+    return all_duplicates_check
 
+
+def extract_duplicates( floor_dct, duplicates ):
+    """ Replaces the list of range-dcts attached to each floor with a floor-level dict of aisle-keys:range-dct-values.
+        Also merges the duplicate-ranges.
+        Called by arrange_metadata_by_floor() """
+    updated_floor_dct = {}
+    for ( floor_key, range_dct_lst ) in floor_dct.items():
+        aisle_dct = {}
+        for range_dct in range_dct_lst:
+            for dup_dct in duplicates:
+                ## if this is one of the duplicates, update duplicates and save a temp-holder to the aisle_dct
+                if range_dct['floor'] == dup_dct['floor'] and range_dct['aisle'] == dup_dct['aisle']:
+                    if 'dup_list' not in dup_dct.keys():
+                        dup_dct['dup_list'] = []
+                    dup_dct['dup_list'].append( range_dct )
+                    try:
+                        temp_holder_dct = {
+                            'aisle': range_dct['aisle'],
+                            'begin': '',
+                            'end': '',
+                            'floor': range_dct['floor'],
+                            'location_code': range_dct['location_code'],
+                            'normalized_start': '',
+                            'note': '',
+                            'padded_aisle': range_dct['padded_aisle']
+                            }
+                    except:
+                        log.exception( 'problem creating temp_holder_dct; traceback follows' )
+                        log.debug( f'problemmatic range_dct, ```{pprint.pformat(range_dct)}```')
+                        raise Exception( 'see logs' )
+                    aisle_dct[range_dct['aisle']] = temp_holder_dct  # so this will happen twice, when the second item is found, but that's ok; the second temp-holder will just overwrite the first.
+                else:
+                    ## if this is NOT one of the duplicates, save the range-info to the aisle_dct
+                    aisle_dct[range_dct['aisle']] = range_dct  # I could pop out the unnecessary 'aisle' element
+        updated_floor_dct[floor_key] = aisle_dct
+        log.debug( f'updated_floor_dct, ```{pprint.pformat(updated_floor_dct)}```' )
+        log.debug( f'enhanced duplicates, ```{pprint.pformat(duplicates)}```' )
+        return ( updated_floor_dct, duplicates )
+
+
+## Old code, when I thought it'd be useful to sort by aisle. No longer necessary; need to trust order of spreadsheet.
+# for ( floor_key, range_dct_lst ) in floor_dct.items():
+#     # sorted_range_dct_lst = sorted( range_dct_lst, key=itemgetter('normalized_start') )
+#     sorted_range_dct_lst = sorted( range_dct_lst, key=itemgetter('padded_aisle', 'normalized_start') )
+#     floor_dct[floor_key] = sorted_range_dct_lst
+
+
+# check_dct = {'floor': range_dct['floor'], 'aisle': range_dct['aisle']}
+# if next( (item for item in duplicates if item  == check_dct), False ) is False:  # <https://stackoverflow.com/a/31988734>
+#     aisle_dct[range_dct['aisle']] = range_dct  # I could pop out the unnecessary 'aisle' element
+# else:
+#     pass
