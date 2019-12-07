5d037dd8a2b9d1755af62ad3fdeb72defb5c592a
birkin <birkin.diana@gmail.com>
2019-12-06 20:09:01 -0500

    removes limit.

--- book_locator_app/lib/label_helper.py
+++ book_locator_app/lib/label_helper.py
@@ -77,9 +77,9 @@ def prep_floor_ranges( sorted_floor_list, initial_dct ):
             elif 'location_code' not in range_info_dct.keys():
                 range_info_dct['location_code'] = 'not_listed'
 
-            # floor_dct[floor].append( range_info_dct )
-            if len( floor_dct[floor] ) < 20:
-                floor_dct[floor].append( range_info_dct )
+            floor_dct[floor].append( range_info_dct )
+            # if len( floor_dct[floor] ) < 20:
+            #     floor_dct[floor].append( range_info_dct )
     log.debug( f'floor_dct, ```{pprint.pformat(floor_dct)}```' )
     return floor_dct
 
